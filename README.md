fe457ed576b51f491458eb9915631faba92eb8f9
gillibrand <jay.gillibrand@gmail.com>
2013-03-26 14:37:12 -0700

    Add some ugly icons.

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 Changes the case of text on the clipboard to uppercase, lowercase, or title case.
 
-[Download the pre-built workflow](http://cl.ly/351y363i1n3B).
+[Download the pre-built workflow](http://cl.ly/2p1x1A0K2d0j).
 
-![Screenshot](change_case.png)
+![Screenshot](changecase.png)
 
 ## Usage
 
 Change the case of text on the keyboard with the keyword `case`. All three styles are previewed as Alfred results. Select one to copy it to the clipboard and paste into in the current application.
 
-Optionally, any text type after `case` will be changed instead of the clipboard. 
+Optionally, any text typed after `case` will be changed instead of the clipboard. 
--- changecase.py
+++ changecase.py
@@ -30,16 +30,19 @@ variations = {
 
 print """<?xml version="1.0"?>
 <items>
-  <item uid="changecase-lower" arg="%(lower)s" type="file">
+  <item uid="changecase-lower" arg="%(lower)s">
     <title>%(lower)s</title>
     <subtitle>Lowercase</subtitle>
+    <icon>lowercase.png</icon>
   </item>
-  <item uid="changecase-upper" arg="%(upper)s" type="file">
+  <item uid="changecase-upper" arg="%(upper)s">
     <title>%(upper)s</title>
     <subtitle>Uppercase</subtitle>
+    <icon>uppercase.png</icon>
   </item>
-  <item uid="changecase-title" arg="%(title)s" type="file">
+  <item uid="changecase-title" arg="%(title)s">
     <title>%(title)s</title>
     <subtitle>Title Case</subtitle>
+    <icon>titlecase.png</icon>
   </item>
 </items>""" % variations
